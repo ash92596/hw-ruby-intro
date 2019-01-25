c9f85ded78035ecc05ba85b19d754aa41deb68b0
ash92596 <ashleigh.presley@faulkner.edu>
2019-01-24 23:33:54 -0600

    3. Object Oriented Basics
    Define a class BookInStock which represents a book with an ISBN number, isbn, and price of the book as a floating-point number, price, as attributes. Run associated tests via: $ rspec -e 'getters and setters' spec/part3_spec.rb
    
    The constructor should accept the ISBN number (a string, since in real life ISBN numbers can begin with zero and can include hyphens) as the first argument and price as second argument, and should raise ArgumentError (one of Ruby's built-in exception types) if the ISBN number is the empty string or if the price is less than or equal to zero. Include the proper getters and setters for these attributes. Run associated tests via: $ rspec -e 'constructor' spec/part3_spec.rb
    
    Include a method price_as_string that returns the price of the book formatted with a leading dollar sign and two decimal places, that is, a price of 20 should format as "$20.00" and a price of 33.8 should format as "$33.80". Run associated tests via: $ rspec -e '#price_as_string' spec/part3_spec.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -70,5 +70,16 @@ end
 # Part 3
 
 class BookInStock
+  attr_accessor :isbn, :price
+  
+  def initialize (isbn,price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    '$%.2f' % price
+  end
   
 end
